73a87304a5001341a9be977d3a6ae2e804253324
Akihiro Naito <neo.akihiro.naito@gmail.com>
2024-11-26 17:11:32 +0900

    a, b commit

---
+++ 2024/11/a.cs
@@ -0,0 +1,28 @@
+﻿namespace monthly_assignments;
+
+class ProgramA
+{
+    static void MainA(string[] args)
+    {
+        var input = Console.ReadLine();
+
+        if (!int.TryParse(input, out var number))
+        {
+            // 入力された文字が数値ではない
+            Console.WriteLine($"数値を入力してください。入力文字：{ input }");
+            return;
+        }
+
+        var isValidOneCount = input.Where(x => x == '1').Count() == 1;
+        var isValidTweCount = input.Where(x => x == '2').Count() == 2;
+        var isValidThreeCount = input.Where(x => x == '3').Count() == 3;
+        if (isValidOneCount && isValidTweCount && isValidThreeCount)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
---
+++ 2024/11/b.cs
@@ -0,0 +1,22 @@
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace monthly_assignments;
+
+class ProgramB
+{
+    static void MainB(string[] args)
+    {
+        var input = Console.ReadLine() ?? "";
+
+        // 先頭の | を削除
+        var target = input[1..];
+
+        // 正規表現
+        var matches = Regex.Matches(target, "-+\\|");
+
+        // 各要素の末尾「|」を除外し、各ハイフンの数を数える
+        var result = matches.Select(x => x.Value[..^1].Where(x => x == '-').Count());
+        Console.WriteLine($"{ string.Join(" ", result) }");
+    }
+}
